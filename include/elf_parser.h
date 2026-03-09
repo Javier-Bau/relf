c3c52dd35c68ce60e7df6ce3103de109e955789a
Javier Bautista <jabautis@ucm.es>
2026-03-09 16:51:54 +0100

    some changes

--- include/elf_parser.h
+++ include/elf_parser.h
@@ -1,6 +1,10 @@
 #ifndef ELF_PARSER_H
 #define ELF_PARSER_H
 
+// Option parser definitions
+#define OPT_EDHR (1 << 0)
+#define OPT_SDHR (1 << 1)
+
 char* get_type_value(unsigned e_type);
 char* get_version_value(unsigned e_version);
 char* get_machine_value(unsigned e_machine);
--- src/main.c
+++ src/main.c
@@ -15,11 +15,17 @@ unsigned char e_ident[EI_NIDENT];
 
 // 64BIT-ELF HEADER
 int is_64_bit;
-Elf64_Ehdr elf64_header;
+Elf64_Ehdr elf64_ehdr;
+Elf64_Shdr elf64_shdr;
 
 // 32BIT-ELF HEADER
 int is_32_bit;
-Elf32_Ehdr elf32_header;
+Elf32_Ehdr elf32_ehdr;
+Elf32_Shdr elf32_shdr;
+
+void print_help(char* filename){
+    fprintf(stderr,"Usage: %s <in_file_name>\n",filename);
+}
 
 void print_magic(unsigned char* magic){ // Imprime completamente el magic en función de su tamaño(Definido por EI_NIDENT)
     for(int i = 0; i < EI_NIDENT; i++)
@@ -42,69 +48,111 @@ int parse_header(){
     is_32_bit = (e_ident[EI_CLASS] == ELFCLASS32);
 
     if(is_32_bit){
-        memcpy(elf32_header.e_ident, e_ident, EI_NIDENT);
-        if (fread(&elf32_header.e_type, sizeof(elf32_header)-EI_NIDENT, 1, i_file) != 1){
+        memcpy(elf32_ehdr.e_ident, e_ident, EI_NIDENT);
+        if (fread(&elf32_ehdr.e_type, sizeof(elf32_ehdr)-EI_NIDENT, 1, i_file) != 1){
             fprintf(stderr, "Error: Unexpected error while trying to read 32bit-elf header.\n");
 	        return 0;
         }
 
     }else if(is_64_bit){
-        memcpy(elf64_header.e_ident, e_ident, EI_NIDENT);
-        if (fread(&elf64_header.e_type, sizeof(elf64_header)-EI_NIDENT, 1, i_file) != 1){
+        memcpy(elf64_ehdr.e_ident, e_ident, EI_NIDENT);
+        if (fread(&elf64_ehdr.e_type, sizeof(elf64_ehdr)-EI_NIDENT, 1, i_file) != 1){
             fprintf(stderr, "Error: Unexpected error while trying to read 64bit-elf header.\n");
 	        return 0;
         }
     }
+    return 1;
 }
 
 void print_header() {
     if(is_32_bit){
         fprintf(stdout, "Header ELF:\n");
         fprintf(stdout, "  Magic Number:");
-        print_magic(elf32_header.e_ident);  
+        print_magic(elf32_ehdr.e_ident);  
         fprintf(stdout, "  Clase: ELF32\n");
-        fprintf(stdout, "  Data Format: %s\n", get_data_value(elf32_header.e_ident[EI_DATA]));
-        fprintf(stdout, "  OS/ABI: %s\n", get_osabi_value(elf32_header.e_ident[EI_OSABI]));
-        fprintf(stdout, "  ABI Version: %u\n", elf32_header.e_ident[EI_ABIVERSION]);
+        fprintf(stdout, "  Data Format: %s\n", get_data_value(elf32_ehdr.e_ident[EI_DATA]));
+        fprintf(stdout, "  OS/ABI: %s\n", get_osabi_value(elf32_ehdr.e_ident[EI_OSABI]));
+        fprintf(stdout, "  ABI Version: %u\n", elf32_ehdr.e_ident[EI_ABIVERSION]);
 
-        fprintf(stdout, "  Type: %s\n", get_type_value(elf32_header.e_type));
-        fprintf(stdout, "  Version: %s\n", get_version_value(elf32_header.e_version));
-        fprintf(stdout, "  Machine: %s\n", get_machine_value(elf32_header.e_machine));
-        fprintf(stdout, "  Entry Point: 0x%04x\n", elf32_header.e_entry);
+        fprintf(stdout, "  Type: %s\n", get_type_value(elf32_ehdr.e_type));
+        fprintf(stdout, "  Version: %s\n", get_version_value(elf32_ehdr.e_version));
+        fprintf(stdout, "  Machine: %s\n", get_machine_value(elf32_ehdr.e_machine));
+        fprintf(stdout, "  Entry Point: 0x%08x\n", elf32_ehdr.e_entry);
     }else{
         fprintf(stdout, "Header ELF:\n");
         fprintf(stdout, "  Magic Number:");
-        print_magic(elf64_header.e_ident);   
+        print_magic(elf64_ehdr.e_ident);   
         fprintf(stdout, "  Clase: ELF64\n");
-        fprintf(stdout, "  Data Format: %s\n", get_data_value(elf64_header.e_ident[EI_DATA]));
-        fprintf(stdout, "  OS/ABI: %s\n", get_osabi_value(elf64_header.e_ident[EI_OSABI]));
-        fprintf(stdout, "  ABI Version: %u\n", elf64_header.e_ident[EI_ABIVERSION]);
+        fprintf(stdout, "  Data Format: %s\n", get_data_value(elf64_ehdr.e_ident[EI_DATA]));
+        fprintf(stdout, "  OS/ABI: %s\n", get_osabi_value(elf64_ehdr.e_ident[EI_OSABI]));
+        fprintf(stdout, "  ABI Version: %u\n", elf64_ehdr.e_ident[EI_ABIVERSION]);
 
 
-        fprintf(stdout, "  Type: %s\n", get_type_value(elf64_header.e_type));
-        fprintf(stdout, "  Version: %s\n", get_version_value(elf64_header.e_version));
-        fprintf(stdout, "  Machine: %s\n", get_machine_value(elf64_header.e_machine));
-        fprintf(stdout, "  Entry Point: 0x%04x\n", elf64_header.e_entry);
+        fprintf(stdout, "  Type: %s\n", get_type_value(elf64_ehdr.e_type));
+        fprintf(stdout, "  Version: %s\n", get_version_value(elf64_ehdr.e_version));
+        fprintf(stdout, "  Machine: %s\n", get_machine_value(elf64_ehdr.e_machine));
+        fprintf(stdout, "  Entry Point: 0x%016lx\n", elf64_ehdr.e_entry);
+        
     }
 }
 
 int main(int argc, char *argv[]){
 
-    if((i_file = fopen("../1", "rb"))== NULL){
+    if(argc == 1){
+        print_help(argv[0]);
+        exit(1);
+    }
+
+    // Start of options parsing
+    char* options_str = "hs";
+    uint8_t options = 0; // 0000 0000
+    /*
+    BIT0: ehdr
+    BIT1: shdr
+    ...
+    */
+
+    int opt;
+    while ((opt = getopt(argc, argv, options_str)) != -1) {
+        char *pos = strchr(options_str, opt);
+
+        if (pos != NULL) {
+            int bit = pos - options_str;
+            options |= (1 << bit);
+        }
+    }
+
+    if(optind >= argc){ // If there is no "input file" argument
+        print_help(argv[0]);
+        exit(1);
+    }
+
+    if((i_file = fopen(argv[optind], "rb"))== NULL){
         fprintf(stderr,"Error while opening input file 1\n");
         exit(1);
     }
 
-    if(parse_header() == 0) return 0;
-
-    int opt;
-    while ((opt = getopt(argc, argv, "h")) != -1) { //Si tiene : después es que quiere un argumento, si no es solo un flag
-        switch(opt) {
-            case 'h':
-                print_header();
-                break;
-        }
+    if(parse_header() == 0){
+        fclose(i_file);
+        exit(1);
     }
 
+    // End of parsing options
+
+    // Executing options
+
+    if(options != 0){
+        if(options & OPT_EDHR)
+            print_header();
+        /*
+        if(options & OPT_SDHR)
+            print_shdr();
+        */
+    }
+    // TODO FUTURE CALLS TO IMPLEMENTATIONS HERE
+
+    fprintf(stdout, "RES: %u" , options);
+
+    fclose(i_file);
     return 0;
 }
